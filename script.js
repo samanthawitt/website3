a83ad149eb004efd33a7212bbf580ab872892a0b
samanthawitt <122926657+samanthawitt@users.noreply.github.com>
2026-05-04 18:57:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,402 @@
+const fishData = [
+  {
+    id: "grayling-fish",
+    name: "Grayling Fish",
+    city: "Shatoolik",
+    img: "grayling.png",
+    info: "The Arctic grayling (Thymallus arcticus) is a species of freshwater fish in the salmon family Salmonidae. T. arcticus is widespread throughout the Arctic and Pacific drainages in Canada, Alaska, and Siberia, as well as the upper Missouri River drainage in Montana. In the U.S. state of Arizona, an introduced population is found in the Lee Valley and other lakes in the White Mountains. They were also stocked at Toppings Lake by the Teton Range and in lakes in the high Uinta Mountains in Utah, as well as alpine lakes of the Boulder Mountains (Idaho) in central Idaho.",
+    containerClass: "graylingimg-container",
+    imgClass: "graylingimg"
+  },
+  {
+    id: "mullet-fish",
+    name: "Mullet Fish",
+    city: "Valletta",
+    img: "mullet.png",
+    info: "The mullets or grey mullets are a family (Mugilidae) of ray-finned fish found worldwide in coastal temperate and tropical waters, and some species in fresh water. Mullets have served as an important source of food in Mediterranean Europe since Roman times. In ancient Egypt, people ate pickled and dried mullet called fesikh.[2][3] The family includes about 78 species in 26 genera. Mullets are distinguished by the presence of two separate dorsal fins, small triangular mouths, and the absence of a lateral line organ. They feed on detritus, and most species have unusually muscular stomachs and a complex pharynx to help in digestion.",
+    containerClass: "mulletimg-container",
+    imgClass: "mulletimg"
+  },
+  {
+    id: "coho-salmon",
+    name: "Coho Salmon",
+    city: "Ivashka",
+    img: "coho.png",
+    info: "The coho salmon (Oncorhynchus kisutch; Karuk: achvuun) is a species of anadromous fish in the salmon family and one of the six Pacific salmon species. Coho salmon are also known as silver salmon (or 'silvers') and is often sold as medium red salmon.The scientific species name is based on the Russian common name kizhuch.",
+    containerClass: "cohoimg-container",
+    imgClass: "cohoimg"
+  },
+  {
+    id: "catfish",
+    name: "Catfish",
+    city: "Chattanooga",
+    img: "catfish.png",
+    info: "Catfish are a diverse group of ray-finned fish of the order Siluriformes (historically Nematognathi). Catfish are named for their prominent barbels, which resemble a cat's whiskers, though not all catfish have prominent barbels. All Siluriformes lack scales, instead possessing either smooth skin or armour-plated bodies. This order of fish are defined by features of the skull (such as maxillae modified into the barbels' bases) and swimbladder. Catfish range in size and behavior from the three largest species alive, the Mekong giant catfish from Southeast Asia, the wels catfish of Eurasia, and the piraíba of South America, to detritivorous and scavenging bottom feeders, down to the tiny ectoparasitic species known as the candiru. In the Southern United States, catfish may be known by a variety of slang names, such as 'mud cat', 'polliwogs', or 'chuckleheads. Such names are regional and unstandardized. For instance, 'chucklehead' in one region may refer to a bullhead catfish but indicate blue catfish elsewhere.",
+    containerClass: "catfishimg-container",
+    imgClass: "catfishimg"
+  },
+  {
+    id: "trout",
+    name: "Trout",
+    city: "St. Louis",
+    img: "trout.png",
+    info: "Trout (pl.: trout) is a generic common name for numerous species of carnivorous freshwater fishes belonging to the genera Oncorhynchus, Salmo, and Salvelinus, all of which are members of the subfamily Salmoninae in the family Salmonidae. The word trout is also used for some similar-shaped but non-salmonid fish, such as the spotted seatrout/speckled trout.",
+    containerClass: "troutimg-container",
+    imgClass: "troutimg"
+  },
+    {
+    id: "pike-fish",
+    name: "Pike Fish",
+    city: "Rheinmünster",
+    img: "pike.png",
+    info: "The northern pike (Esox lucius), also known as pickerel, jack, northern or simply pike, is a species of carnivorous fish of the genus Esox (pikes). It is commonly found in fresh waters of the Northern Hemisphere (i.e. it has a holarctic distribution).",
+    containerClass: "pikeimg-container",
+    imgClass: "pikeimg"
+  },
+ {
+    id: "sawshark",
+    name: "Sawshark",
+    city: "Nassau",
+    img: "sawshark.png",
+    info: "A sawshark or saw shark is a member of a shark order (Pristiophoriformes /prɪstiˈɒfɒrɪfɔːrmiːz/) bearing a unique long, saw-like rostrum (snout or bill) edged with sharp teeth, which they use to slash and disable their prey. There are ten species within the Pristiophoriformes. Sawsharks are found in many areas around the world, most commonly in waters from the Indian Ocean to the southern Pacific Ocean. They are normally found at depths around 40–100 m, but can be found much lower in tropical regions. The Bahamas sawshark was discovered in deeper waters (640 m to 915 m) of the northwestern Caribbean.",
+    containerClass: "sawsharkimg-container",
+    imgClass: "sawsharkimg"
+  },
+   {
+    id: "zebra-shark",
+    name: "Zebra Shark",
+    city: "Jeddah",
+    img: "zebrashark.png",
+    info: "The zebra shark (Stegostoma tigrinum) is a species of carpet shark and the only extant member of the family Stegostomatidae. It is found throughout the tropical Indo-Pacific, frequenting coral reefs and sandy flats to a depth of 62 m (200 ft). Zebra sharks are distinctive in appearance, with adults possessing five longitudinal ridges on a cylindrical body, a low caudal fin comprising nearly half the total length, and typically a pattern of dark spots on a pale background. Young zebra sharks under 50–90 cm (20–35 in) long have a completely different pattern, consisting of light vertical stripes on a brown background, and lack the ridges. This species attains a length of 2.5 m (8.2 ft).",
+    containerClass: "zebrasharkimg-container",
+    imgClass: "zebrasharkimg"
+  },
+ {
+    id: "blacktip-shark",
+    name: "Blacktip Shark",
+    city: "Tampico",
+    img: "blacktip.png",
+    info: "The blacktip reef shark (Carcharhinus melanopterus) is a species of requiem shark, in the family Carcharhinidae, which can be easily identified by the prominent black tips on its fins (especially on the first dorsal fin and its caudal fin). Among the most abundant sharks inhabiting the tropical coral reefs of the Indian and Pacific Oceans, this species prefers shallow, inshore waters. Its exposed first dorsal fin is a common sight in the region. The blacktip reef shark is usually found over reef ledges and sandy flats, though it has also been known to enter brackish and freshwater environments. It typically attains a length of 1.6 m (5.2 ft). Like other sharks, the females are larger than the males.",
+    containerClass: "blacktipimg-container",
+    imgClass: "blacktipimg"
+  },
+   {
+    id: "whale-shark",
+    name: "Whale Shark",
+    city: "Conakry",
+    img: "whaleshark.png",
+    info: "The whale shark (Rhincodon typus) is a slow-moving, filter-feeding carpet shark and the largest known extant fish species. An individual with a length of 18.8 m (61.7 ft) has been considered the largest reliably recorded.[8] The whale shark holds many records for size in the animal kingdom, most notably being by far the largest living non-cetacean animal. It is the only living species of the genus Rhincodon and the only extant member of the family Rhincodontidae, which belongs to the subclass Elasmobranchii in the class Chondrichthyes. Before 1984, it was classified as Rhiniodon into Rhinodontidae. An extinct species, Rhincodon ferriolensis, was described in Europe (probably in Spain), in the Late Burdigalian stage.",
+    containerClass: "whalesharkimg-container",
+    imgClass: "whalesharkimg"
+  },
+  {
+    id: "perch-fish",
+    name: "Perch Fish",
+    city: "Stockholm",
+    img: "perch.png",
+    info: "Perch is a common name for freshwater fish from the genus Perca, which belongs to the family Percidae of the large order Perciformes. The name comes from Greek: πέρκη, romanized: perke, meaning the type species of this genus, the European perch (P. fluviatilis).",
+    containerClass: "perchimg-container",
+    imgClass: "perchimg"
+  }
+];
+
+fishData.forEach(fishie => {
+  const fish = document.createElement("div");
+  fish.classList.add("fish");
+  fish.setAttribute("data-city", fishie.city);
+  fish.dataset.fishData = JSON.stringify({ name: fishie.name, info: fishie.info });
+
+  fish.innerHTML = `
+    <div class="fishimg-container ${fishie.containerClass}" id="${fishie.id}" draggable="true">
+      <img src="${fishie.img}" alt="${fishie.name} fish" class="${fishie.imgClass}">
+    </div>
+    <div class="fishinfo">
+      <h2>The ${fishie.name}</h2>
+      <div class="fish-weather"></div>
+      <button class="infobutton">click to learn more!</button>
+      <button class="fishbowldelete">click to remove from your fishbowl</button>
+    </div>
+  `;
+
+  document.body.appendChild(fish);
+});
+
+async function getWeather(city) {
+    try {
+        let geoResponse = await fetch(`https://cse2004.com/api/geocode?address=${city}`)
+        let geoData = await geoResponse.json()
+
+        let lat = geoData.results[0].geometry.location.lat
+        let lng = geoData.results[0].geometry.location.lng
+
+        let weatherResponse = await fetch(`https://cse2004.com/api/weather?latitude=${lat}&longitude=${lng}`)
+        let weather = await weatherResponse.json()
+        return weather
+    } catch (error) {
+        console.error("Weather fetch failed:", error);
+        return null;
+    }
+}
+
+document.querySelectorAll('.fish').forEach(fish => {
+    const imgContainer = fish.querySelector('.fishimg-container');
+    const infoBox = fish.querySelector('.fishinfo');
+    const weatherText = fish.querySelector('.fish-weather');
+    const city = fish.dataset.city; 
+    const thisinfobutton = fish.querySelector('.infobutton')
+    const deletebutton = fish.querySelector('.fishbowldelete'); 
+
+    thisinfobutton.addEventListener('click', (e) => {
+        e.stopPropagation();
+        information.classList.toggle('informed');
+        if (information.classList.contains('informed')) {
+            information.style.display = 'block';
+            updateAside();
+        } else{
+            information.style.display = 'none';
+        }
+        });
+
+    imgContainer.addEventListener('click', async (e) => {
+        e.stopPropagation();
+        const isAlreadyClicked = fish.classList.contains('clicked');
+        document.querySelectorAll('.fish').forEach(f => {
+            f.classList.remove('clicked');
+            f.querySelector('.fishinfo').style.display = 'none';
+        });
+
+        if (!isAlreadyClicked) {
+            fish.classList.add('clicked');
+            
+            document.body.classList.add('fish-is-active');
+
+            const weather = await getWeather(city);
+            if (weather) {
+                weatherText.textContent = `Current temp in this fish's habitat: ${weather.feelsLikeTemperature.degrees}°`;
+            } else {
+                weatherText.textContent = 'Current weather information is currently unavailable.';
+            }
+
+            display(fish, imgContainer, infoBox);
+            const dataToSave = JSON.parse(fish.dataset.fishData);
+            saveData(dataToSave);
+
+            if (information.classList.contains('informed')) {
+                updateAside();
+            }
+        } else {
+            document.body.classList.remove('fish-is-active');
+        }
+
+        if (information.classList.contains('informed')) {
+                thisinfobutton.style.display = 'none';
+                updateAside();
+            } else {
+                thisinfobutton.style.display = 'block';
+            }
+    });
+
+    deletebutton.addEventListener('click', (e) => {
+        e.stopPropagation(); // Prevent the fish from "clicking" behind the button
+        const fishId = imgContainer.id;
+        selectedFish = selectedFish.filter(id => id !== fishId);
+        localStorage.setItem('myAquarium', JSON.stringify(selectedFish));
+        imgContainer.style.display = 'none';
+        console.log(`${fishId} removed from bowl.`);
+    });
+
+});
+
+function display(fish, fishimgcontainer, fishinfo) {
+    if (fish.classList.contains('clicked')) {
+        fishinfo.style.display = 'block';
+        const rect = fishimgcontainer.getBoundingClientRect();
+        const infoHeight = fishinfo.offsetHeight;
+        const spacing = 50;
+
+        let left = (rect.left + window.scrollX + (rect.width / 2)) - (fishinfo.offsetWidth / 2);
+        
+        if (left + fishinfo.offsetWidth > window.innerWidth) {
+            left = window.innerWidth - (fishinfo.offsetWidth + 20);
+        }
+        if (left < 0) left = 10; 
+
+        const spaceBelow = window.innerHeight - rect.bottom;
+        let top;
+
+        if (spaceBelow < infoHeight + spacing) {
+            top = rect.top + window.scrollY - infoHeight - spacing;
+        } else {
+            top = rect.bottom + window.scrollY + spacing;
+        }
+
+        fishinfo.style.top = `${top}px`;
+        fishinfo.style.left = `${left}px`;
+    } else {
+        fishinfo.style.display = 'none';
+    }
+}
+
+document.addEventListener('click', () => {
+    document.querySelectorAll('.fish').forEach(fish => {
+        fish.classList.remove('clicked');
+        fish.querySelector('.fishinfo').style.display = 'none';
+        
+        const btn = fish.querySelector('.infobutton');
+        if(btn) btn.style.display = 'block';
+    });
+    document.body.classList.remove('fish-is-active');
+
+    if (!information.classList.contains('sticky')) {
+        information.classList.remove('informed');
+        information.style.display = 'none';
+    }
+});
+
+const moreinfo = document.querySelector('.moreinfo')
+const fishbowl = document.querySelector('.fishbowl')
+const information = document.querySelector('.information')
+const target = document.querySelector('.thefishbowl')
+
+moreinfo.addEventListener('click', () => {
+    information.classList.toggle('informed');
+    information.classList.toggle('sticky');
+
+        if (information.classList.contains('informed')) {
+            information.style.display = 'block';
+            updateAside();
+            document.querySelectorAll('.infobutton').forEach(btn => btn.style.display = 'none');
+        } else{
+            information.style.display = 'none';
+            document.querySelectorAll('.infobutton').forEach(btn => btn.style.display = 'block');
+        }
+});
+
+function saveData(fishinfo){
+    localStorage.setItem('lastClickedItem', JSON.stringify(fishinfo));
+}
+
+function updateAside() {
+    const fishname = document.querySelector('.fishiename');
+    const fishieinfo = document.querySelector('.fishieinfo');
+    const savedData = localStorage.getItem('lastClickedItem');
+
+    if (savedData) {
+        const lastfish = JSON.parse(savedData);
+        fishname.textContent = lastfish.name;
+        fishieinfo.textContent = lastfish.info;
+    }
+    else {
+        fishname.textContent = "Click on a fish to learn more!"; }
+}
+
+const closeBtn = document.querySelector('.close-info');
+
+closeBtn.addEventListener('click', (e) => {
+    e.stopPropagation(); 
+
+    information.classList.remove('informed');
+    information.classList.remove('sticky');
+    information.style.display = 'none';
+
+    document.querySelectorAll('.infobutton').forEach(btn => {
+        btn.style.display = 'block';
+    });
+});
+
+let selectedFish = JSON.parse(localStorage.getItem('myAquarium')) || [];
+
+target.addEventListener('dragover', (e) => {
+    e.preventDefault(); 
+});
+
+target.addEventListener('drop', (e) => {
+    e.preventDefault();
+
+    const fishId = e.dataTransfer.getData('text/plain');
+    
+    if (!selectedFish.includes(fishId)) {
+        selectedFish.push(fishId);
+        localStorage.setItem('myAquarium', JSON.stringify(selectedFish));
+        alert("Fish added to your bowl!");
+    } else {
+        alert("Fish already added to your bowl!");
+    }
+
+    target.style.display = 'none';
+});
+
+document.addEventListener('dragstart', (e) => {
+    const fishContainer = e.target.closest('.fishimg-container');
+    
+    if (fishContainer) {
+        e.dataTransfer.setData('text/plain', fishContainer.id);
+        target.style.display = 'flex'; 
+    }
+});
+
+document.addEventListener('dragend', () => {
+    target.style.display = 'none';
+});
+
+const h1 = document.querySelector('h1');
+
+let currentUserName = localStorage.getItem('aquariumName');
+
+if (!currentUserName) {
+    currentUserName = prompt("Welcome to the Aquarium! What is your name?");
+    if (currentUserName) {
+        localStorage.setItem('aquariumName', currentUserName);
+    }
+}
+
+fishbowl.addEventListener('click', () => {
+    fishbowl.classList.toggle('onthebowl');
+    target.style.display = 'none'; 
+
+    selectedFish = JSON.parse(localStorage.getItem('myAquarium')) || [];
+    const deletebutton = document.querySelectorAll('.fishbowldelete');
+
+    if (fishbowl.classList.contains('onthebowl')) {
+
+        if (currentUserName) {
+            h1.textContent = `[ ${currentUserName}'S F I S H B O W L ]`; 
+        }
+        else {
+            h1.textContent = "[ Y O U R   F I S H B O W L ]";
+        }
+
+        deletebutton.forEach(btn => btn.style.display = 'block');
+
+        fishData.forEach(fish => {
+            const element = document.querySelector(`.${fish.containerClass}`);
+            
+            if (element) {
+                if (!selectedFish.includes(fish.id)) {
+                    element.style.display = 'none';
+                } else {
+                    element.style.display = 'block'; 
+                }
+            }
+        });
+
+        h1.addEventListener('dblclick', () => {
+        const newName = prompt("Change your aquarium name:", currentUserName);
+    
+        if (newName !== null && newName.trim() !== "") {
+            currentUserName = newName;
+            localStorage.setItem('aquariumName', newName);
+            updateTitle(newName);
+        }
+        fishbowl.textContent = "sam's aquarium";    
+    });
+    } else {
+        h1.textContent = "( S A M ' S   A Q U A R I U M )";
+        fishData.forEach(fish => {
+            const element = document.querySelector(`.${fish.containerClass}`);
+            if (element) element.style.display = 'block';
+        });
+        deletebutton.forEach(btn => btn.style.display = 'none');
+        fishbowl.textContent = "fishbowl"; 
+    }
+});
